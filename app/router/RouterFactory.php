ac068afcec4cc2495d98d765c118a57befe55d31
bouchja1 <jan.bouchner@gmail.com>
2017-02-13 00:19:54 +0100

    mofification

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -30,47 +30,6 @@ final class RouterFactory
         $this->createFrontModuleRouter();
 
         return $this->router;
-
-        /*
-		$router = new RouteList;
-
-        // Setup router
-        $router[] = $frontRouter = new RouteList('Front');
-
-        $frontRouter[] = new Route('<presenter=Homepage>/<action=default>', array(
-            'presenter' => array(
-                Route::FILTER_TABLE => array(
-                    'kontakt' => 'Contact',
-                    'historie' => 'About',
-                    'lokalita' => 'Location',
-                    'tabor' => 'Camp'
-                )),
-            'action' => 'default',
-            'title' => array(
-                Route::VALUE => NULL,
-                Route::FILTER_IN => function($url) {
-                    return Strings::webalize($url);
-                },
-                Route::FILTER_OUT => function($url) {
-                    return Strings::webalize($url);
-                }
-            ),
-            'id' => NULL
-                ), NULL);
-
-        $router[] = $adminRouter = new RouteList('Admin');
-
-        $adminRouter[] = new Route('admin/<presenter=Homepage>/<action=default>', array(
-            'presenter' => 'Homepage',
-            'action' => 'default',
-            'id' => NULL,
-                ), NULL);
-
-        $router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
-        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
-
-        return $router;
-        */
 	}
 
     private function createAdminModuleRouter() {
